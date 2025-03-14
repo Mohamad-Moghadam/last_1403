32ea98f0f2238528ac39797d4794c48988df5672
farnaz <farnaz.radaei@gmail.com>
2025-03-14 10:30:01 +0330

    ✨ url added

--- golhaye_kelaasor/bottle/urls.py
+++ golhaye_kelaasor/bottle/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from bottle.views import sign_up, new_message
+from bottle.views import sign_up, new_message , list_messages
 
 urlpatterns = [
     path('sign-up', sign_up),
-    path('new-message', new_message)
+    path('new-message', new_message),
+    path('list-message', list_messages)
 ]
